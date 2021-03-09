09f76f3c01feedeed903dccca799cde882cbde02
Amanda Rodrigues <amandahrodrigues@outlook.com>
2021-03-09 11:11:17 -0300

    Adiciona arquivos do local para o remoto

---
+++ agenda_contatos/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:br/ui/contact_page.dart';
+import 'package:br/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
